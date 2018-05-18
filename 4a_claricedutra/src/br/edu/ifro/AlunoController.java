898d27a3423f922300b787f1eaab29f9612cdd0a
01118108256 <01118108256@JIPA-LABIN05-02.ifro.local>
2018-05-18 07:54:49 -0400

    Alterando o projeto.

---
+++ 4a_claricedutra/src/br/edu/ifro/AlunoController.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ifro;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+/**
+ * FXML Controller class
+ *
+ * @author 01118108256
+ */
+public class AlunoController implements Initializable {
+
+    @FXML
+    private TextField txtNome;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void salvar(ActionEvent event) {
+    }
+
+    @FXML
+    private void fechar(ActionEvent event) {
+            Stage stage = (Stage) txtNome.getScene().getWindow();
+        stage.close();
+}
+    }
+
--- 4a_claricedutra/src/br/edu/ifro/MenuPrincipalController.java
+++ 4a_claricedutra/src/br/edu/ifro/MenuPrincipalController.java
@@ -5,14 +5,19 @@
  */
 package br.edu.ifro;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
+import javafx.event.Event;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 /**
  *
@@ -25,6 +30,14 @@ public class MenuPrincipalController implements Initializable {
 
     @FXML
     private Button btnSoma;
+    @FXML
+    private Label label;
+    @FXML
+    private Label lblResultado;
+    @FXML
+    private Label lblNumero2;
+    @FXML
+    private Label lblNumero1;
     
     @FXML
     private void soma(ActionEvent event) {
@@ -39,6 +52,26 @@ public class MenuPrincipalController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void abrirCadastroAluno(ActionEvent event) {
+        try{
+    FXMLLoader fxmlLoader = new FXMLLoader();
+    fxmlLoader.setLocation(getClass().getResource("Aluno.fxml"));
+    Scene scene = new Scene(fxmlLoader.load(),400,400);
+    Stage stage = new Stage();
+    stage.setTitle("Cadastrar aluno");
+    stage.setScene(scene);
+    stage.show();
+        }
+        catch(IOException e){
+
+        }
+    }
+
+    @FXML
+    private void fecharProjeto(Event event) {
+    }
     
 }
 
